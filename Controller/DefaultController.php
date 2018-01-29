50a59a175765de14de98806c612036bedea2a7a6
Piou-piou <pilloud.anthony@gmail.com>
2018-01-29 22:13:33 +0100

    [func] add testing default route

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class DefaultController extends Controller {
 	/**
-	 * @Route("/index/", name="ribsblog_index")
+	 * @Route("/index/", name="ribsadmin_blog_index")
 	 * @return Response
 	 */
 	public function indexAction(): Response {
