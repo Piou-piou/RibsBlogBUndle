93d9544f79e419f37c4bbd5855ead466b7d9cfa6
Piou-piou <pilloud.anthony@gmail.com>
2018-02-03 11:46:39 +0100

    [add] templates files

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -6,13 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
-class DefaultController extends Controller {
+class DefaultController extends Controller
+{
 	/**
 	 * @Route("/index/", name="ribsadmin_blog_index")
 	 * @return Response
 	 */
-	public function indexAction(): Response {
-		return new Response("test blog ok");
+	public function indexAction(): Response
+	{
+		return $this->render("@RibsBlog/admin/index.html.twig");
 	}
 }
