7ebc92f46fa8c84c1aa39969aaee1d24b064281d
Piou-piou <pilloud.anthony@gmail.com>
2018-02-04 12:51:29 +0100

    [func] add getter and setters for State entity

--- Entity/State.php
+++ Entity/State.php
@@ -5,7 +5,7 @@ namespace PiouPiou\RibsBlogBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * RibsmoduleBlogState
+ * State
  *
  * @ORM\Table(name="ribsmodule_blog_state")
  * @ORM\Entity
@@ -27,6 +27,36 @@ class State
      * @ORM\Column(name="state", type="string", length=255, nullable=false)
      */
     private $state;
-
-
+	
+	/**
+	 * @return int
+	 */
+	public function getId(): int
+	{
+		return $this->id;
+	}
+	
+	/**
+	 * @param int $id
+	 */
+	public function setId(int $id)
+	{
+		$this->id = $id;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getState(): string
+	{
+		return $this->state;
+	}
+	
+	/**
+	 * @param string $state
+	 */
+	public function setState(string $state)
+	{
+		$this->state = $state;
+	}
 }
